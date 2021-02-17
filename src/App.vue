5f3870a8c41a36aa9bd59a2f7e94936113d39e9e
miller21m <miller05488@gmail.com>
2021-02-17 19:20:27 +0200

    init commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,57 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="m_homePage" >
+    <div class="m_getButton" @click="fetchData">
+      Fetch Data
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import axios from 'axios'
 export default {
-  name: 'App',
-  components: {
-    HelloWorld
+  methods:{
+    fetchData(){
+
+      axios.get('https://jsonplaceholder.typicode.com/posts')
+      .then((results)=>{
+
+        const newResult = results.data.map(result =>{
+          //check if id us even
+          if(result.id % 2 == 0){
+            //If id even then convert to upperCase and return the updated object to the new array
+            result.title = result.title.toUpperCase()
+            return result;
+          }else{
+            return result;
+          }
+        });
+        console.log(newResult);//Log all the updated results
+      })
+      .catch((err)=>{
+        console.log(err);
+      })
+    }
   }
 }
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style scoped>
+  .m_homePage{
+    display: flex;
+  }
+
+  .m_getButton{
+    cursor: pointer;
+    border: 1px solid black;
+    border-radius: 35px;
+    
+    padding: 6px;
+    display: flex;
+    margin: 20px auto;
+  }
+
+  .m_getButton:hover{
+    background: blue;
+    color: white;
+  }
 </style>
